3dc0694464c7acef70acdf8b685292c15997a39c
Stefano Benedetti <stefano.benedetti21@gmail.com>
2026-03-04 01:24:46 +0100

    aggiunto plugin backend e una semplice schermata di registrazione e login

---
+++ book-power/backend/script/login.gd
@@ -0,0 +1,29 @@
+extends Control
+
+func _ready():
+	Firebase.Auth.login_succeeded.connect(_on_FirebaseAuth_login_succeeded)
+	Firebase.Auth.signup_succeeded.connect(_on_FirebaseAuth_login_succeeded)
+	Firebase.Auth.login_failed.connect(on_login_failed)
+	Firebase.Auth.signup_failed.connect(on_signup_failed)
+
+func _on_login_pressed():
+	var email = $email.text
+	var password = $password.text
+	Firebase.Auth.login_with_email_and_password(email, password)
+
+func _on_register_pressed():
+	var email = $email.text
+	var password = $password.text
+	Firebase.Auth.signup_with_email_and_password(email, password)
+
+func _on_FirebaseAuth_login_succeeded(auth):
+		# You do not need to call get_user_data() here, as auth is the same variable
+	print(auth)
+	
+func on_login_failed(error_code, message):
+	print("error code: " + str(error_code))
+	print("message: " + str(message))
+
+func on_signup_failed(error_code, message):
+	print("error code: " + str(error_code))
+	print("message: " + str(message))
