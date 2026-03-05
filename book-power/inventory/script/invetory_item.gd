58e2ee8024e6cec527f36352ba506d6cd322154f
Simone <enomis000000gmail.com.com>
2026-03-05 23:44:29 +0100

    aggiunto libro di analisi e aggiunta la scena dell'attacco del libro di analisi, va ora effettivamente creato l'attacco

--- book-power/inventory/script/invetory_item.gd
+++ book-power/inventory/script/invetory_item.gd
@@ -1,10 +1,11 @@
 extends Resource
 
-#script per modellare gli elementi dell'inventario
-#negli altri oggetti per avere associati un oggetto che può andare nell'inventario
-#bisognerà fare @export var nome_che_useremo_nello_script: InvItem
+#script per modellare gli la reource degli elementi dell'inventario
+#negli altri oggetti per avere associati un oggetto che può andare
+#nell'inventario bisognerà fare
+#@export var nome_che_useremo_nello_script: InvItem
 
-#definiamo la classe a cui apparterrà ogni oggetto dell'inventario
+#definiamo la classe a cui apparterrà ogni ItemData dell'inventario
 #ogni oggetto dell'inventario avrà un nome e una texture
 class_name InvItem
 @export var name: String = ""
---
+++ book-power/script/libro_analisi.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+@export var item: InvItem	#va messo perchè è un oggetto collezionabile
+
+var player_in_area = false
+var player = null
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if player_in_area:
+		if Input.is_action_just_pressed("Pick_object"):
+			var has_picked = player.collect(item)
+			if(has_picked):
+				queue_free()
+
+
+func _on_pickable_area_body_entered(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_in_area = true
+		player = body
+
+
+func _on_pickable_area_body_exited(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_in_area = false
