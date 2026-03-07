d2a7b30038b5b80272348966dba39a89197554e5
Stefano Benedetti <stefano.benedetti21@gmail.com>
2026-03-07 15:04:22 +0100

    sistemati tutti i menu aggiunti fin'ora, forse li miglioreremo

--- book-power/art/script/login.gd
+++ book-power/art/script/login.gd
@@ -7,13 +7,13 @@ func _ready():
 	Firebase.Auth.signup_failed.connect(on_signup_failed)
 
 func _on_login_pressed():
-	var email = $email.text
-	var password = $password.text
+	var email = $VBoxContainer/email.text
+	var password = $VBoxContainer/password.text
 	Firebase.Auth.login_with_email_and_password(email, password)
 
 func _on_register_pressed():
-	var email = $email.text
-	var password = $password.text
+	var email = $VBoxContainer/email.text
+	var password = $VBoxContainer/password.text
 	Firebase.Auth.signup_with_email_and_password(email, password)
 	
 func _on_back_pressed():
