6a119f4c3dc72f5519e9324dc323ce5fbaf6c3a0
Stefano <158204304+Stefano-Benedetti@users.noreply.github.com>
2026-03-18 15:27:55 +0100

    sistemato codice login (pochissimo) e inserita scena casella_dialogo con script
    lo script di casella_dialogo va perfezionato per collegarlo con gli NPC e con le quest

--- book-power/script/casella_dialogo.gd
+++ book-power/script/casella_dialogo.gd
@@ -1,11 +1,48 @@
 extends Control
 
+@onready var label = $PanelContainer/MarginContainer/Label
 
-# Called when the node enters the scene tree for the first time.
+var index = 0
+
+var quest_corrente = 0
+
+var dialoghi_per_quest := {
+	0: [
+		"Ciao avventuriero!",
+		"Puoi raccogliere 3 mele per me?"
+	],
+	1: [
+		"Grazie per le mele!",
+		"Ora portale al cuoco in città."
+	],
+	2: [
+		"Ottimo lavoro!",
+		"Ci vediamo al prossimo livello."
+	]
+}
+
 func _ready() -> void:
-	pass # Replace with function body.
+	avvia_dialogo_quest(quest_corrente)
+
+func avvia_dialogo_quest(quest_stage: int) -> void:
+	quest_corrente = quest_stage
+	index = 0
+	$PanelContainer.show()
+	mostra_riga_corrente()
+
+func mostra_riga_corrente() -> void:
+	var righe: Array = dialoghi_per_quest.get(quest_corrente,["..."])
+	label.text = righe[index]
+
+func _on_next_pressed() -> void:
+	var righe: Array = dialoghi_per_quest.get(quest_corrente,["..."])
+	if index < righe.size() - 1:
+		index += 1
+		mostra_riga_corrente()
+	else:
+		$PanelContainer.hide()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_next_quest_pressed() -> void:
+	quest_corrente += 1
+	avvia_dialogo_quest(quest_corrente)
