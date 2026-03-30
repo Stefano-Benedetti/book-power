9c648a51de98910ee6df0f26f8eb6f9a92720428
Simone <enomis000000gmail.com.com>
2026-03-30 17:30:47 +0200

    modificata mappa02, aggiunta la leva, aggiunto cancello (forse da rivedere)

---
+++ book-power/script/leva.gd
@@ -0,0 +1,33 @@
+extends Node2D
+
+
+var player_in_area = false
+var player = null
+@export var funzionante: bool
+
+@export var one_shot: bool	#per indicare se la leva può essere ritirata più volte
+var tirata = false	#per indicare se la leva è stata tirata
+
+signal leva_tirata
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	if not funzionante:
+		return
+	if one_shot and tirata:
+		return
+	if player_in_area:
+		if Input.is_action_just_pressed("Pick_object"):
+			$Sprite2D.flip_h = true
+			tirata = true
+			leva_tirata.emit()
+
+
+func _on_area_of_interaction_body_entered(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_in_area = true
+		player = body
+
+func _on_area_of_interaction_body_exited(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_in_area = false
