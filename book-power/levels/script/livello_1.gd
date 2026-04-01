427aceed38e4cb95b0963e00a68ee46dcbe4ff85
Simone <enomis000000gmail.com.com>
2026-04-01 23:49:07 +0200

    aggiunta barriera a mappa02, aggiunta schermata di caricamento, aggiunta collisione a NPC, modificata mappa02

--- book-power/levels/script/livello_1.gd
+++ book-power/levels/script/livello_1.gd
@@ -36,5 +36,7 @@ func removeRoadblock():
 
 func _on_to_next_level_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
 	if body.has_method("player"):
+		if not get_parent():
+			return
 		get_parent().loadNextLevel()
 		#get_tree().call_deferred("change_scene_to_file", "res://levels/scenes/livello_2.tscn")
