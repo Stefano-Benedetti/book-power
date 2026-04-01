32f13690be78b18409b8c5ace559b463a83b354f
Simone <enomis000000gmail.com.com>
2026-04-02 00:01:20 +0200

    tolti diversi commenti inutili dai codici e messa la pssobilità di avviare game partendo da qualsiasi livello si vuole (via codice)

--- book-power/script/game.gd
+++ book-power/script/game.gd
@@ -1,53 +1,6 @@
-#extends Node2D
-#
-#@onready var livelli: Array
-#@onready var livello_corrente: PackedScene = null
-#@onready var scena_liv_corrente: Node = null
-#
-## Called when the node enters the scene tree for the first time.
-#func _ready() -> void:
-	#livelli = [
-		#preload("res://levels/scenes/livello_1.tscn"),
-		#preload("res://levels/scenes/livello_2.tscn")
-	#]
-	#loadLevel(1)
-#
-#
-#
-## Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-	#pass
-#
-#
-#
-#func loadLevel(num):
-	#$loading_screen.show()
-	#await get_tree().create_timer(1.0).timeout
-	#livello_corrente = livelli[num-1]
-	#scena_liv_corrente = livello_corrente.instantiate()
-	#add_child(scena_liv_corrente)
-	#$loading_screen.hide()
-#
-#
-#func loadNextLevel():
-	#get_tree().paused = true
-	#$loading_screen.show()
-	#
-	#await get_tree().create_timer(1.0).timeout
-	#
-	#scena_liv_corrente.queue_free()
-	#
-	#var index = livelli.find(livello_corrente)
-	#livello_corrente = livelli[index+1]
-	#
-	#scena_liv_corrente = livello_corrente.instantiate()
-	#add_child(scena_liv_corrente)
-	#
-	#$loading_screen.hide()
-	#get_tree().paused = false
-
 extends Node2D
 
+@export var livello_partenza: int = 1
 @onready var livelli: Array
 @onready var livello_corrente: PackedScene = null
 @onready var scena_liv_corrente: Node = null
@@ -58,7 +11,7 @@ func _ready() -> void:
 		preload("res://levels/scenes/livello_1.tscn"),
 		preload("res://levels/scenes/livello_2.tscn")
 	]
-	loadLevel(0) # carica il primo livello (indice 0)
+	loadLevel(livello_partenza-1) # carica il primo livello (indice 0)
 
 
 func loadLevel(indice: int) -> void:
