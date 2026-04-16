0f8ba1dd4d20f7d3703b100d1314bf3805326e05
Stefano <158204304+Stefano-Benedetti@users.noreply.github.com>
2026-04-16 15:13:37 +0200

    inserita mappa04bis in mappa04 e aggiustate alcune dinamiche di lvl4

--- book-power/levels/script/livello_4.gd
+++ book-power/levels/script/livello_4.gd
@@ -8,6 +8,8 @@ var talked_with_computer = false
 
 var player_in_ap_area = false
 
+var player_in_cave_entrance = false
+
 var player
 
 func _ready() -> void:
@@ -21,6 +23,7 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if QuestCounter.quest_corrente == 7:
 		if player_in_ap_area and Input.is_action_just_pressed("Pick_object") and player.inv.countItem(required_item)>0:
+			endUseOfCounter()
 			QuestCounter.quest_corrente = 8
 			player.consumeItem(required_item,1)
 			if object_ap == null:
@@ -28,8 +31,12 @@ func _process(delta: float) -> void:
 			var scena_dropped_object = object_ap.instantiate()
 			add_child(scena_dropped_object)
 			scena_dropped_object.global_position = Vector2(927.802,-576.853)
+			#scena_dropped_object.play...
 			Global.emit_signal("removeFireWall")
 			object_ap = null
+	if player_in_cave_entrance and player.current_dir == "up":
+		player.position = Vector2(0,0)
+		player_in_cave_entrance = false
 
 func talkedWithComputer():
 	if not talked_with_computer:
@@ -57,3 +64,18 @@ func _on_place_router_body_entered(body: Node2D) -> void:
 	if body.has_method("player"):
 		player_in_ap_area = true
 		player = body
+
+func _on_place_router_body_exited(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_in_ap_area = false
+		player = null
+		
+func _on_cave_entrance_body_entered(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_in_cave_entrance = true
+		player = body
+
+func _on_cave_entrance_body_exited(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_in_cave_entrance = false
+		player = null
