4007995c70a2d2d89ba549c76e2b19949abef790
Simone <enomis000000@gmail.com>
2026-05-13 14:00:22 +0200

    sistemato cancello del livello 2

--- book-power/objects_of_scene/script/cancello.gd
+++ book-power/objects_of_scene/script/cancello.gd
@@ -5,6 +5,8 @@ var opened = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$AnimatedSprite2D.animation = "open"
+	$AnimatedSprite2D.frame = 0
 	Global.leva_tirata.connect(open)
 
 
