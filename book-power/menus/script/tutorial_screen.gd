3f64f50a044c6e5132b37f2c620c4c0e7163a278
Stefano <158204304+Stefano-Benedetti@users.noreply.github.com>
2026-05-13 21:18:55 +0200

    risolto bug player immobile

--- book-power/menus/script/tutorial_screen.gd
+++ book-power/menus/script/tutorial_screen.gd
@@ -4,7 +4,6 @@ extends CanvasLayer
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	self.hide()
-	GameState.in_tutorial = true
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
